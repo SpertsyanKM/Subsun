ac83a3d15a6791237bd6d490957224fe35cf38fa
SCpRoION <spertsiankamo@gmail.com>
2021-11-07 22:47:50 +0300

    Empty state small styling.

--- app/src/main/java/ru/lefty/subsun/ui/subscriptionList/SubscriptionList.kt
+++ app/src/main/java/ru/lefty/subsun/ui/subscriptionList/SubscriptionList.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.icons.outlined.Subscriptions
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import ru.lefty.subsun.R
@@ -28,9 +29,20 @@ fun EmptySubscriptionList() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(Icons.Outlined.Subscriptions, contentDescription = "", modifier = Modifier
-            .width(50.dp)
-            .height(50.dp))
-        Text(text = stringResource(R.string.subscriptions_empty_state))
+        Icon(
+            Icons.Outlined.Subscriptions,
+            contentDescription = "Subscriptions",
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.subscriptions_empty_state),
+            modifier = Modifier.padding(
+                horizontal = dimensionResource(id = R.dimen.padding_xl),
+                vertical = dimensionResource(id = R.dimen.padding_xs)
+            )
+        )
     }
 }
